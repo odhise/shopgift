012e7ff9f88e63b792a6194106db09e859f61ab7
odhise <evansodhis491@gmail.com>
2021-11-05 08:00:47 +0300

    complete some part of shopgift project

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const mySelect=document.getElementById("select")
+const mySelecton=document.getElementById("selection")
+const mySelector=document.getElementById("selector")
+const mySelectify=document.getElementById("selectify")
+
+const myItems=document.getElementById("items")
+const myIdenty=document.getElementById("identy")
+const myClass=document.getElementById("class")
+const myMyc=document.getElementById("myc")
+
+mySelect.addEventListener("click",()=>{
+alert("welcome")
+})
